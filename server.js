9364e53a6e3ac2a44014e6dbed3ed340967f64ad
hammadsatti080 <hammadsatti080@gmail.com>
2026-06-17 06:49:14 -0700

    new updated

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-   origin: "https://ecommerence-bay.vercel.app",
+   origin: "https://farm-backend-lac.vercel.app",
    // origin: "http://localhost:3000",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
 }));
